3d865f58031d37ae696e0c84ef78056cbbdfe365
baublytevaiva <baublytevaiva@gmail.com>
2020-03-06 20:38:57 +0200

    updates

--- contacts.php
+++ contacts.php
@@ -1,15 +1,36 @@
-
 <?php include "header.php" ?>
 
 <!-- Čia kontaktų puslapio vidinė dalis -->
 
-<h1>How to arrive?</h1>
+<div class="container">
+    <h1>How to arrive?</h1>
 
-<p>
-Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam dolore facere animi amet eos, distinctio aliquid accusantium blanditiis recusandae corrupti. Sint dolorem nihil numquam ipsam praesentium quae obcaecati et modi.lorem
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero impedit corrupti soluta dolorum. Repudiandae rem nam praesentium molestiae nostrum libero tempora doloremque at rerum deserunt sequi, suscipit, autem eveniet.
-</p>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam dolore facere animi amet eos, distinctio aliquid accusantium blanditiis recusandae corrupti. Sint dolorem nihil numquam ipsam praesentium quae obcaecati et modi.lorem
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero impedit corrupti soluta dolorum. Repudiandae rem nam praesentium molestiae nostrum libero tempora doloremque at rerum deserunt sequi, suscipit, autem eveniet.
+    </p>
 
+    <div class= "flexbox">
 
+    <div>
+        <img src="https://picsum.photos/201" alt="">
 
+    </div>
+
+    <div>
+        <img src="https://picsum.photos/201" alt="">
+
+    </div>
+
+    <div>
+        <img src="https://picsum.photos/201" alt="">
+    </div>
+
+    </div>
+
+</div>
+
+<div class = "googlemap">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d35931.591335214056!2d25.768808780664443!3d55.74591638273078!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46e80279d0ee5d7d%3A0x36b9628b2bfb2ab4!2sSartai%20Regional%20Park!5e0!3m2!1sen!2slt!4v1583517547113!5m2!1sen!2slt" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+</div>
 <?php include "footer.php" ?>
