8dba5ab99f29e7ff4dcc32509caf79492829128d
SarunasBudrevicius <b.sarunas@gmail.com>
2020-03-06 21:17:23 +0200

    Paskutiniai pakeitimai

--- objektas.php
+++ objektas.php
@@ -5,14 +5,14 @@
     <h1>Pažintinis takas2</h1>
 
     <p class="red-text">Ąžuolynas</p>
-
-    <p>Šį pažintinį taką rasti pakankamai paprasta, važiuojant nuo Vilniaus link Kervavės, pravažiavus Dūkštas ir už jų esančią lomą, ant kalno, dešinėje pusėje, stovi nuoroda į šį taką. Sprendžiant pagal automobilių skaičių, pažintinis takas pakankamai populiarus, o geru oru net ir aikštelės nepakanka, todėl automobiliais būna nustatyta visa šalikelė.</p>
-</div>
+    </div>
 <div class="flexbox">
     <div>
-         <img src="https://picsum.photos/200" alt="">
+         <img class="foto-dydis" src="foto/azuolynas.jpg" alt="">
     </div>
 </div>
+    <p>Šį pažintinį taką rasti pakankamai paprasta, važiuojant nuo Vilniaus link Kervavės, pravažiavus Dūkštas ir už jų esančią lomą, ant kalno, dešinėje pusėje, stovi nuoroda į šį taką. Sprendžiant pagal automobilių skaičių, pažintinis takas pakankamai populiarus, o geru oru net ir aikštelės nepakanka, todėl automobiliais būna nustatyta visa šalikelė.</p>
+
 
     <?php include "footer.php" ?>
 
