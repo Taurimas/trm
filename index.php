361b8276f61e8f6313999657afac258ea14def3d
Taurimas <ssvinkunas@gmail.com>
2020-03-09 19:23:58 +0200

    +

--- index.php
+++ index.php
@@ -35,6 +35,11 @@
   </div>
 </div>
 
+<div class="row">
+      <div class="col s1">1</div>
+      <div class="col s1">2</div>
+      <div class="col s1">3</div>
+
   </div>
 
 <?php include "footer.php" ?>
