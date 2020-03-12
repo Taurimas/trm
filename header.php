a7f32f8475f57de1a36d0800ccfd2d6e4849a88b
Taurimas <ssvinkunas@gmail.com>
2020-03-12 19:52:15 +0200

    +

--- header.php
+++ header.php
@@ -15,10 +15,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Masonry Layout con CSS Grid</title>
+    
     
 
-    <title>Projekto pavadinimas</title>
+    <title>Pažintinais takais</title>
 </head>
 
 
